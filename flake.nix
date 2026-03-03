2744a851e4b68e93f126f8bfe98b49c029fa57bf
eleocraft <eliseo.cailloux@gmx.de>
2026-03-03 12:31:40 +0100

    building via naersk

--- flake.nix
+++ flake.nix
@@ -8,15 +8,21 @@
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.fenix.follows = "fenix";
+    };
   };
 
-  outputs = { self, nixpkgs, fenix, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ fenix.overlays.default ];
         };
+        rust-toolchain = pkgs.fenix.stable.toolchain;
         deps = with pkgs; [
           webkitgtk_6_0
           gtk4
@@ -24,20 +30,36 @@
           gdk-pixbuf
           cairo
           glib
-          pkg-config
         ];
       in
       {
         devShells.default =
-        let
-          rust-pkgs = pkgs.fenix.stable.toolchain;
-        in
         pkgs.mkShell {
           buildInputs = with pkgs; [
-            rust-pkgs
-            rust-analyzer-nightly
+            rust-toolchain
+            rust-analyzer
+
+            # extra cargo tools
+            cargo-edit
+            cargo-expand
+
+            # pkg config
+            pkg-config
           ] ++ deps;
+
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath deps;
+          # set the rust src for rust_analyzer
+          RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
+        };
+
+        packages.default = 
+        (naersk.lib.${system}.override {
+          cargo = rust-toolchain;
+          rustc = rust-toolchain;
+        }).buildPackage {
+          src = ./.;
+          buildInputs = deps;
+          nativeBuildInputs = [ pkgs.pkg-config pkgs.autoPatchelfHook ];
         };
       }
     );
